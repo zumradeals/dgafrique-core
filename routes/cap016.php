da618824f70f7479f8542a8f45292ed88a0aab42
zumradeals <zumradeals@gmail.com>
2026-08-15 21:27:58 +0000

    Fix CAP-016 web session middleware

--- routes/cap016.php
+++ routes/cap016.php
@@ -6,18 +6,20 @@ use App\Http\Controllers\Administration\ProjectAccompanimentController as Admini
 use App\Http\Controllers\ProjectAccompanimentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/projets/{project}/accompagnement', [ProjectAccompanimentController::class, 'show'])
-    ->whereUuid('project')->middleware('core.member')->name('projects.accompaniment.show');
-Route::post('/projets/{project}/accompagnement', [ProjectAccompanimentController::class, 'activate'])
-    ->whereUuid('project')->middleware(['core.member', 'throttle:project-accompaniment'])->name('projects.accompaniment.activate');
-Route::put('/projets/{project}/accompagnement/fin', [ProjectAccompanimentController::class, 'end'])
-    ->whereUuid('project')->middleware(['core.member', 'throttle:project-accompaniment'])->name('projects.accompaniment.end');
+Route::middleware('web')->group(function (): void {
+    Route::get('/projets/{project}/accompagnement', [ProjectAccompanimentController::class, 'show'])
+        ->whereUuid('project')->middleware('core.member')->name('projects.accompaniment.show');
+    Route::post('/projets/{project}/accompagnement', [ProjectAccompanimentController::class, 'activate'])
+        ->whereUuid('project')->middleware(['core.member', 'throttle:project-accompaniment'])->name('projects.accompaniment.activate');
+    Route::put('/projets/{project}/accompagnement/fin', [ProjectAccompanimentController::class, 'end'])
+        ->whereUuid('project')->middleware(['core.member', 'throttle:project-accompaniment'])->name('projects.accompaniment.end');
 
-Route::prefix('administration')->middleware(['core.member', 'portal.admin'])->group(function (): void {
-    Route::get('/accompagnement-projets', [AdministrationProjectAccompanimentController::class, 'edit'])
-        ->name('administration.project-accompaniment.edit');
-    Route::put('/accompagnement-projets', [AdministrationProjectAccompanimentController::class, 'update'])
-        ->middleware('throttle:project-accompaniment-configuration')->name('administration.project-accompaniment.update');
-    Route::post('/accompagnement-projets/{project}/interventions', [AdministrationProjectAccompanimentController::class, 'storeAction'])
-        ->whereUuid('project')->middleware('throttle:project-accompaniment-admin')->name('administration.project-accompaniment.actions.store');
+    Route::prefix('administration')->middleware(['core.member', 'portal.admin'])->group(function (): void {
+        Route::get('/accompagnement-projets', [AdministrationProjectAccompanimentController::class, 'edit'])
+            ->name('administration.project-accompaniment.edit');
+        Route::put('/accompagnement-projets', [AdministrationProjectAccompanimentController::class, 'update'])
+            ->middleware('throttle:project-accompaniment-configuration')->name('administration.project-accompaniment.update');
+        Route::post('/accompagnement-projets/{project}/interventions', [AdministrationProjectAccompanimentController::class, 'storeAction'])
+            ->whereUuid('project')->middleware('throttle:project-accompaniment-admin')->name('administration.project-accompaniment.actions.store');
+    });
 });
