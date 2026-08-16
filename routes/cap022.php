9d7388dcba74d1c81d62eb17f77ad8644b7535e6
zumradeals <zumradeals@gmail.com>
2026-08-16 16:08:19 +0000

    CAP-022 — Add contextual sharing routes

---
+++ routes/cap022.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Controllers\ContextShareController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('web')->group(function (): void {
+    Route::get('/partages', [ContextShareController::class, 'index'])
+        ->middleware(['core.member', 'throttle:shares-read'])
+        ->name('shares.index');
+
+    Route::get('/partages/zumra/{group}', [ContextShareController::class, 'group'])
+        ->whereUuid('group')
+        ->middleware(['core.member', 'throttle:shares-read'])
+        ->name('shares.group');
+
+    Route::get('/partages/besoins/{need}', [ContextShareController::class, 'need'])
+        ->whereUuid('need')
+        ->middleware(['core.member', 'throttle:shares-read'])
+        ->name('shares.need');
+
+    Route::post('/partages/besoins/{need}', [ContextShareController::class, 'storeNeed'])
+        ->whereUuid('need')
+        ->middleware(['core.member', 'throttle:shares-write'])
+        ->name('shares.need.store');
+
+    Route::get('/partages/projets/{project}', [ContextShareController::class, 'project'])
+        ->whereUuid('project')
+        ->middleware(['core.member', 'throttle:shares-read'])
+        ->name('shares.project');
+
+    Route::post('/partages/projets/{project}', [ContextShareController::class, 'storeProject'])
+        ->whereUuid('project')
+        ->middleware(['core.member', 'throttle:shares-write'])
+        ->name('shares.project.store');
+});
