1743ba611c05927cc984b61c1a1e0c77f376db89
zumradeals <zumradeals@gmail.com>
2026-08-18 21:50:37 +0000

    test: align ZUMRA honest empty state with redesigned hub

--- tests/Feature/MemberAccountFlowTest.php
+++ tests/Feature/MemberAccountFlowTest.php
@@ -118,7 +118,7 @@ final class MemberAccountFlowTest extends TestCase
         $this->get('/zumra')
             ->assertOk()
             ->assertSee('ZUMRA')
-            ->assertSee('Vous n’êtes membre d’aucune ZUMRA')
+            ->assertSee('Votre première ZUMRA vous attend')
             ->assertDontSee('Contribution à jour')
             ->assertDontSee('24 J’aime');
     }
