8d2ae24e33be6fb6ebc51d33505b6065625cc628
zumradeals <zumradeals@gmail.com>
2026-08-18 21:39:19 +0000

    Allow page-scoped Vite styles in portal layout

--- resources/views/components/layouts/portal.blade.php
+++ resources/views/components/layouts/portal.blade.php
@@ -1,3 +1,7 @@
+@props([
+    'title' => 'DG Afrique',
+    'styles' => [],
+])
 <!doctype html>
 <html lang="fr">
 <head>
@@ -5,9 +9,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="robots" content="noindex, nofollow">
-    <title>{{ $title ?? 'DG Afrique' }}</title>
+    <title>{{ $title }}</title>
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(array_merge(['resources/css/app.css'], (array) $styles, ['resources/js/app.js']))
     @endif
 </head>
 <body class="portal-body">
