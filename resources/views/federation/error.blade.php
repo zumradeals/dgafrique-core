fa1f7f0f50cd820ef1c99597533b88ff171236e0
zumradeals <zumradeals@gmail.com>
2026-09-08 13:20:43 +0000

    fix(federation): align error state with current UI contract

--- resources/views/federation/error.blade.php
+++ resources/views/federation/error.blade.php
@@ -1,10 +1,13 @@
 <x-layouts.public title="Impossible de poursuivre">
     <div class="flex min-h-[100svh] items-center justify-center px-5 py-10">
         <x-dg.state
-            tone="danger"
+            type="error"
             title="Nous n’avons pas pu poursuivre vers ce service"
-            :description="$message"
-            :action="['label' => 'Revenir à GAMAD', 'href' => route('gateway')]"
-        />
+            :message="$message"
+        >
+            <x-slot:actions>
+                <x-dg.button :href="route('gateway')">Revenir à GAMAD</x-dg.button>
+            </x-slot:actions>
+        </x-dg.state>
     </div>
 </x-layouts.public>
